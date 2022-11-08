438c697b68dd00e57cba9715cd668f8b367a26a8
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2022-11-08 22:51:03 +0100

    added footer to layout

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,10 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+
+const Footer = () => (
+  <Flex align="center" justifyContent="center">
+    <Box>Copyright 2022 Zsolt Mester</Box>
+  </Flex>
+);
+
+export default Footer;
--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -1,14 +1,33 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
 
+import Footer from "./Footer";
 import NavBar from "./NavBar";
 
 const Layout = ({ routes, children }) => (
-  <Grid backgroundColor="primary.grey" templateAreas={`"header" "main"`}>
+  <Grid templateAreas={`"header" "main" "footer"`}>
     <GridItem area="header">
       <NavBar routes={routes} />
     </GridItem>
-    <GridItem area="main">{children}</GridItem>
+    <GridItem
+      area="main"
+      position="relative"
+      minHeight="100vh"
+      paddingBottom="2.5rem"
+      backgroundColor="primary.grey"
+    >
+      {children}
+    </GridItem>
+    <GridItem
+      area="footer"
+      position="absolute"
+      bottom={0}
+      width="100%"
+      height="2.5rem"
+      backgroundColor="primary.blue"
+    >
+      <Footer />
+    </GridItem>
   </Grid>
 );
 
