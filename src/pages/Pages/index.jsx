b38b9b667becea6ee0b7754f34b1d32ca8c05f27
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2022-11-13 17:20:44 +0100

    added page icons

--- src/pages/Pages/index.jsx
+++ src/pages/Pages/index.jsx
@@ -1,32 +1,81 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Link, Text } from "@chakra-ui/react";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+import rollingKidsImage from "../../assets/img/pages/rolling-kids.jpg";
 
 const pageItems = [
   {
     title: "Ukraine 2022",
+    url: "/pages/ukraine-2022",
+    backgroundImage: rollingKidsImage,
   },
   {
-    title: "Rolling kids",
+    isExternal: true,
+    title: "Rolling Kids",
+    url: "https://rollingkids.eu/",
+    backgroundImage: rollingKidsImage,
   },
   {
     title: "Kresz Park",
+    url: "/pages/kresz-park",
+    backgroundImage: rollingKidsImage,
   },
   {
     title: "Cipősdoboz",
+    url: "/pages/ciposdoboz",
+    backgroundImage: rollingKidsImage,
   },
 ];
+
+const PageImage = ({ title, backgroundImage }) => (
+  <Flex flexDirection="column" gap={3} alignItems="center">
+    <Flex
+      backgroundImage={`url(${backgroundImage})`}
+      backgroundSize="cover"
+      borderRadius="10px"
+      backgroundColor="lightgrey"
+      opacity="90%"
+      height={{ base: "250px", md: "320px" }}
+      width="100%"
+      minWidth="250px"
+      maxWidth="320px"
+      _hover={{ opacity: "50%" }}
+    />
+    <Text
+      fontWeight={700}
+      fontSize="24px"
+      _hover={{ textDecoration: "underline" }}
+    >
+      {title}
+    </Text>
+  </Flex>
+);
 
 const Pages = () => (
   <Grid
     gap={8}
-    m="50px"
     color="black"
-    align="center"
-    templateColumns={{ base: "repeat(2, 1fr)" }}
-    backgroundColor="lightgrey"
+    m={{
+      base: "25px",
+      lg: "50px 100px 50px",
+      xl: "50px 250px 50px",
+      "2xl": "50px 500px 50px",
+    }}
+    templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
   >
-    {pageItems.map((item, idx) => (
-      <GridItem key={`page-${idx}`}>{item.title}</GridItem>
+    {pageItems.map(({ title, backgroundImage, url, isExternal }, idx) => (
+      <GridItem key={`page-${idx}`}>
+        {isExternal ? (
+          <a href={url} target="_blank" rel="noreferrer">
+            <PageImage title={title} backgroundImage={backgroundImage} />
+          </a>
+        ) : (
+          <Link as={RouterLink} to={url}>
+            <PageImage title={title} backgroundImage={backgroundImage} />
+          </Link>
+        )}
+      </GridItem>
     ))}
   </Grid>
 );
