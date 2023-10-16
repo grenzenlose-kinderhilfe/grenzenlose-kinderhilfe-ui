827e35d2388407b0e351e4ce1d849d9143ce07c9
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2023-10-16 21:40:44 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: "build/",
+    rollupOptions: {
+      output: {
+        assetFileNames: "static/[ext]/[name]-[hash][extname]",
+        entryFileNames: "static/js/[name]-[hash].js",
+      },
+    },
+  },
 })
