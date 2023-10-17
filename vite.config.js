09eaaa64cb771738387945908c93c3697e60d110
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2023-10-17 20:43:37 +0200

    added zip for dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import zipPack from 'vite-plugin-zip-pack'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), zipPack({ outFileName: 'grenzenlose-kinderhilfe.zip' })],
   build: {
-    outDir: 'build/',
     rollupOptions: {
       output: {
         assetFileNames: 'static/[ext]/[name]-[hash][extname]',
