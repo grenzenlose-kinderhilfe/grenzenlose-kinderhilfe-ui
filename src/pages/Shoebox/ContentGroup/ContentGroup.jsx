ee63c7c99106cb1daf4ab91333f7669a49423295
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2023-12-03 20:12:12 +0100

    video embed

--- src/pages/Shoebox/ContentGroup/ContentGroup.jsx
+++ src/pages/Shoebox/ContentGroup/ContentGroup.jsx
@@ -8,6 +8,7 @@ import {
   Text
 } from '@chakra-ui/react'
 import React from 'react'
+import { Trans } from 'react-i18next'
 
 const ContentGroup = () => (
   <Grid
@@ -27,17 +28,33 @@ const ContentGroup = () => (
         flexDirection='column'
         justifyContent='center'
       >
-        <Heading as='h1' fontSize='32px' maxW='600px'>
-          Legyen minden karacsony boldog
-        </Heading>
-        <Text maxW='800px' fontStyle='italic'>
-          "
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-          "
-        </Text>
+        <Trans i18nKey='pages.shoebox.contentLeft'>
+          <Heading as='h1' fontSize='2rem' maxW='600px'>
+            Cipősdoboz Akció a Gyermekekért
+          </Heading>
+          <Text>
+            Adományozz és Teremtsünk Együtt Karácsonyi Csodát!
+          </Text>
+          <Text maxW='800px' fontStyle='italic'>
+            Gondoltad volna, hogy egy egyszerű cipősdoboz képes varázsolni? Ráadásul gyermekmosolyokat!
+            Karitatív akciónk során célunk, hogy minél több rászoruló gyermek számára hozzunk létre boldog
+            pillanatokat az év végi ünnepek alatt.
+          </Text>
+          <Heading as='h3' fontSize='1.75rem'>
+            Csatlakozz Hozzánk!
+          </Heading>
+          <Text fontStyle='italic'>
+            Az akció része lenni, azért jó, mert nem csak az adományozásról szól, hanem az összetartozásról is.
+            Csatlakozz a Grenzenlose Kinderhilfe közösségéhez, ahol együtt tehetünk azért, hogy minél több
+            gyermek életét tegyük boldoggá.
+          </Text>
+          <Text fontStyle='italic'>
+            Mindannyian tudjuk, hogy egy kis figyelem, szeretet és gondoskodás mennyire feldobhatja egy
+            gyermek napját. Az évente megrendezésre Cipősdoboz Akció célja pontosan ez: örömet és reményt
+            vinni a rászoruló gyerekek számára. Ez már a 7. év, és ez alatt az idő alatt közel 13.000 gyermeket
+            sikerült megajándékozni.
+          </Text>
+        </Trans>
       </Flex>
     </GridItem>
     <GridItem
--- src/pages/Shoebox/Fundraiser/Fundraiser.jsx
+++ src/pages/Shoebox/Fundraiser/Fundraiser.jsx
@@ -2,15 +2,26 @@ import {
   AspectRatio,
   Grid,
   GridItem,
-  useMediaQuery
+  useMediaQuery,
+  Heading,
+  Text,
+  Flex
 } from '@chakra-ui/react'
 import React from 'react'
+import { LOCALE_DE } from '../../../constants'
+import { Trans, useTranslation } from 'react-i18next'
 
 const Fundraiser = () => {
   const [isLargerThan321] = useMediaQuery('(min-width: 321px)')
 
+  const { i18n } = useTranslation()
+
   const iframeVariant = isLargerThan321 ? 'medium' : 'small'
   const iframeMaxHeight = isLargerThan321 ? '240px' : '50px'
+
+  const shoeboxMarketingEmbedSource = i18n.language === LOCALE_DE
+    ? 'https://www.youtube.com/embed/h__srWvtSK4?si=6SPuhV09jUNmORT_'
+    : 'https://www.youtube.com/embed/tIL6BPiSY1w?si=JxAeMR7Gop2wV2k-'
 
   return (
     <Grid
@@ -21,18 +32,36 @@ const Fundraiser = () => {
       <GridItem
         alignSelf='center'
       >
-        <AspectRatio width='100%' maxWidth='500px' maxHeight={iframeMaxHeight} ratio={1}>
+        <AspectRatio width='100%' ratio={16 / 9}>
           <iframe
-            class='gfm-embed-iframe' frameBorder={0} scrolling='no'
-            src={`https://www.gofundme.com/f/help-baby-mario-beat-an-aggressive-cancer/widget/${iframeVariant}#:~:tcm-prompt=Hidden`}
+            title='Határtalan Segítség Alapítvány'
+            width='1268'
+            height='713'
+            src={shoeboxMarketingEmbedSource}
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
           />
         </AspectRatio>
       </GridItem>
       <GridItem alignSelf='center'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        <Flex flexDirection='column' alignItems='center' gap={8} maxW='600px'>
+          <Trans i18nKey='pages.shoebox.donateActionText'>
+            <Heading as='h1' fontsize='2rem'>
+              Adományozz most és támogasd a szállítmányozást!
+            </Heading>
+          </Trans>
+          <AspectRatio width='100%' maxWidth='500px' maxHeight={iframeMaxHeight} ratio={1}>
+            <iframe
+              class='gfm-embed-iframe' frameBorder={0} scrolling='no'
+              src={`https://www.gofundme.com/f/help-baby-mario-beat-an-aggressive-cancer/widget/${iframeVariant}#:~:tcm-prompt=Hidden`}
+            />
+          </AspectRatio>
+          <Trans i18nKey='asd'>
+            <Text mt='-1rem'>
+              Adományoddal te is részese lehetsz ennek az örömteli kezdeményezésnek. Kattints ide és csatlakozz
+              a nagyobb jó ügyéhez.
+            </Text>
+          </Trans>
+        </Flex>
       </GridItem>
     </Grid>
   )
