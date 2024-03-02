54edf4754a22e601f33d4430d95cf64e8cb45ba5
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2024-03-02 14:21:11 +0100

    add infinite loading to media page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./AppRoutes";
 import Layout from "./components/ui/Layout";
 import SuspenseBoundary from "./components/ui/SuspenseBoundary";
 import routes from "./routes";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   const displayedRoutes = routes.filter((route) => route?.display ?? true);
 
   return (
-    <BrowserRouter>
-      <Layout routes={displayedRoutes}>
-        <SuspenseBoundary>
-          <AppRoutes routes={routes} />
-        </SuspenseBoundary>
-      </Layout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout routes={displayedRoutes}>
+          <SuspenseBoundary>
+            <AppRoutes routes={routes} />
+          </SuspenseBoundary>
+        </Layout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
--- src/pages/Media/index.jsx
+++ src/pages/Media/index.jsx
@@ -2,23 +2,74 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
 
 import mediaItems from "./mediaItems.json";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import withTracker from "../../utils/tracking";
 import MediaItem from "./MediaItem/MediaItem";
 
+const MEDIA_CHUNK_SIZE = 4;
+
 const Media = () => {
+  const observer = React.useRef();
+
   const sortedMediaItems = React.useMemo(
     () => mediaItems.sort((a, b) => Date.parse(b.date) - Date.parse(a.date)),
     [],
   );
 
+  const chunks = sortedMediaItems.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / MEDIA_CHUNK_SIZE);
+
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [];
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+  }, []);
+
+  const fetchMedia = async ({ pageParam = 0 }) =>
+    chunks.length > pageParam ? chunks[pageParam] : [];
+
+  const { data, fetchNextPage, hasNextPage, isFetching, isLoading } =
+    useInfiniteQuery({
+      queryKey: ["media"],
+      queryFn: fetchMedia,
+      getNextPageParam: (lastPage, allPages) =>
+        lastPage.length ? allPages.length : undefined,
+    });
+
+  const lastElementRef = React.useCallback(
+    (node) => {
+      if (isLoading) return;
+
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+          fetchNextPage();
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextPage, hasNextPage, isFetching, isLoading],
+  );
+
+  const flatData = React.useMemo(
+    () => data?.pages.reduce((acc, current) => [...acc, ...current], []),
+    [data],
+  );
+
   return (
     <Grid color="black" m="25px" gap={8}>
-      {sortedMediaItems.map((item, idx) => (
-        <GridItem key={idx}>
-          <MediaItem {...item} />
-        </GridItem>
-      ))}
+      {flatData &&
+        flatData.map((item, idx) => (
+          <GridItem key={idx} ref={lastElementRef}>
+            <MediaItem {...item} />
+          </GridItem>
+        ))}
     </Grid>
   );
 };
